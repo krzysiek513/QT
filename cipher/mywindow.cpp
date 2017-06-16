7e0294f1e25a31cb1679aa93d9a39c2439db0e67
jokr <krzysiek3155@gmail.com>
2017-06-16 15:00:38 +0200

    layout created using desinger

--- cipher/mywindow.cpp
+++ cipher/mywindow.cpp
@@ -6,9 +6,18 @@ MyWindow::MyWindow(QWidget *parent) :
     ui(new Ui::MyWindow)
 {
     ui->setupUi(this);
+    enRB = new QRadioButton(this);
+    cryText = new QTextEdit();
+
+    QObject::connect(enRB,SIGNAL(pressed()),qApp,SLOT(encrypt()));
 }
 
 MyWindow::~MyWindow()
 {
     delete ui;
+}
+
+void MyWindow::encrypt()
+{
+    cryText->setText("dupa");
 }
--- cipher/mywindow.h
+++ cipher/mywindow.h
@@ -1,6 +1,9 @@
 #ifndef MYWINDOW_H
 #define MYWINDOW_H
 
+#include <QRadioButton>
+#include <QTextEdit>
+
 #include <QMainWindow>
 
 namespace Ui {
@@ -14,9 +17,14 @@ class MyWindow : public QMainWindow
 public:
     explicit MyWindow(QWidget *parent = 0);
     ~MyWindow();
+    void encrypt();
+    void decrypt();
 
 private:
     Ui::MyWindow *ui;
+    QRadioButton *enRB;
+    QRadioButton *deRB;
+    QTextEdit  *cryText;
 };
 
 #endif // MYWINDOW_H
