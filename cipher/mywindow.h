0d63ac9ca05ec543846cd22a2501a41aba082259
jokr <krzysiek3155@gmail.com>
2017-06-16 13:55:05 +0200

    loyout prepared

---
+++ cipher/mywindow.h
@@ -0,0 +1,22 @@
+#ifndef MYWINDOW_H
+#define MYWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MyWindow;
+}
+
+class MyWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MyWindow(QWidget *parent = 0);
+    ~MyWindow();
+
+private:
+    Ui::MyWindow *ui;
+};
+
+#endif // MYWINDOW_H
