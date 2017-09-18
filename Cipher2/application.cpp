efe41d67e0b85221522647fdcd8f6dd420f2b5d4
Krzysiek <krzysiek3155@gmail.com>
2017-09-18 21:02:25 +0200

    layout created

--- Cipher2/application.cpp
+++ Cipher2/application.cpp
@@ -2,34 +2,49 @@
 
 application::application():QWidget()
 {
-    box=new QGroupBox();
+    mainText=new QTextEdit(this);
+
+    box=new QGroupBox(this);
+    crypting=new QRadioButton("crypting",box);
+    encrypting=new QRadioButton("decrypting",box);
+
+    VBox=new QVBoxLayout();
+    VBox->addWidget(crypting);
+    VBox->addWidget(encrypting);
+
+    box->setMaximumWidth(100);
+    box->setWindowModified(false);
+    box->setLayout(VBox);
+
     form=new QFormLayout();
     key=new QLineEdit;
     path=new QLineEdit;
     grid=new QGridLayout(this);
+
+    ok=new QPushButton("ok");
+
     showkey=new QPushButton("..");
+    showkey->setMaximumSize(16,16);
     showpath=new QPushButton("<");
+    showpath->setMaximumSize(16,16);
     form->addRow("enter the key",key);
-    form->addRow("eter path",path);
+    form->addRow("enter path",path);
+    form->setVerticalSpacing(15);
+    form->setHorizontalSpacing(10);
+    form->setMargin(5);
 
 
-
-    grid->addWidget(showkey,0,4,1,1);
-    grid->addWidget(showpath,1,4,1,1);
-    grid->addWidget(box,0,0,2,1);
-    grid->addLayout(form,0,1,3,1);
-
+    grid->setMargin(10);
+    grid->setHorizontalSpacing(0);
+    grid->addWidget(showkey,0,6,1,1);
+    grid->addWidget(showpath,1,6,1,1);
+    grid->addWidget(box,0,0,2,2);
+    grid->addLayout(form,0,2,2,4);
+    grid->addWidget(ok,3,2,1,2);
+    grid->addWidget(mainText,4,0,1,7);
 
 
     grid=new QGridLayout(this);
 
 
-//    setLayout(grid);
-
-
-//    Vlayout.addItem(&key);
-//    Vlayout.addItem(&path);
-
-
-//    setLayout(&Vlayout);
 }
--- Cipher2/application.h
+++ Cipher2/application.h
@@ -8,20 +8,29 @@ class application : public QWidget
 public:
     application();
 private:
+
+    bool crypt;
+    bool showPass;
+
     QGroupBox *box;
+    QRadioButton *crypting;
+    QRadioButton *encrypting;
 
     QLineEdit *key;
     QLineEdit *path;
 
+    QTextEdit *mainText;
+
     QPushButton *showkey;
     QPushButton *showpath;
+    QPushButton *ok;
 
     ///layouts
     QGridLayout *grid;
     QFormLayout *form;
-    QHBoxLayout Hlayout1;
-    QHBoxLayout Hlayout2;
-    QVBoxLayout Vlayout;
+//    QHBoxLayout Hlayout1;
+//    QHBoxLayout Hlayout2;
+    QVBoxLayout *VBox;
 
 };
 #endif // APPLICATION_H
